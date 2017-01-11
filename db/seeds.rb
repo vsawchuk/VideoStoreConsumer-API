a537eae0a6be1e1f82d70e46ed2a3375ce0d07b2
Dan Roberts <dan@adadevelopersacademy.org>
2017-01-11 15:34:22 -0800

    Write script to seed database from JSON files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+customer_data = JSON.parse(File.read('db/seeds/customers.json'))
+
+customer_data.each do |customer|
+  Customer.create!(customer)
+end
+
+JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
+  Movie.create!(movie)
+end
