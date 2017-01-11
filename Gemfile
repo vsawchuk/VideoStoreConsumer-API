7b5d67266dfea648ce62e26489886ad6ccb82196
Dan Roberts <dan@adadevelopersacademy.org>
2017-01-11 14:22:10 -0800

    Make Rails use minitest spec style
    And write a couple tests to verify it works

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ group :development do
 end
 
 group :test do
+  gem 'minitest-spec-rails'
   gem 'minitest-reporters'
 end
 
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  let (:customer_data) {
+    {
+      "name": "Ada Testington III",
+      "registered_at": "Wed, 29 Apr 2015 07:54:14 -0700",
+      "address": "12345 Ada Ave E",
+      "city": "Seattle",
+      "state": "WA",
+      "postal_code": "98107",
+      "phone": "(123) 456-7890",
+      "account_credit": 13.15
+    }
+  }
+  let (:customer) { Customer.new(@customer_data) }
+
+  describe "Constructor" do
+    it "Can be created" do
+      Customer.create!(@customer_data)
+    end
+
+    it "Has rentals" do
+      @customer.must_respond_to :rentals
+    end
+  end
 end
