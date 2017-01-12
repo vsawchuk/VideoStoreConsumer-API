2d1e513b6e98fe7c9ef3973505b3fc64f4cdf7a4
Dan Roberts <dan@adadevelopersacademy.org>
2017-01-12 13:21:59 -0800

    Build sorting for /customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,27 @@
 class CustomersController < ApplicationController
+  SORT_FIELDS = %w(name registered_at postal_code)
+
+  before_action :parse_query_args
+
   def index
-    render json: Customer.all.as_json(
+    if @sort
+      data = Customer.all.order(@sort)
+    else
+      data = Customer.all
+    end
+    
+    render json: data.as_json(
       only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit],
       methods: [:movies_checked_out_count]
     )
+  end
+
+private
+  def parse_query_args
+    @sort = params[:sort]
+    if @sort and not SORT_FIELDS.include? @sort
+      render status: :bad_request, json: { errors: { sort: "Invalid sort field '#{@sort}'" } }
+      return
+    end
   end
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -19,6 +19,7 @@ class CustomersControllerTest < ActionDispatch::IntegrationTest
 
       data = JSON.parse @response.body
       data.each do |customer|
+        customer.must_include "id"
         customer.must_include "name"
         customer.must_include "registered_at"
         customer.must_include "postal_code"
@@ -43,6 +44,70 @@ class CustomersControllerTest < ActionDispatch::IntegrationTest
       data.each do |customer|
         expected_names[customer["name"]].must_equal false
         expected_names[customer["name"]] = true
+      end
+    end
+
+    describe "sorting" do
+      it "can sort by name" do
+        get customers_url, params: { sort: 'name' }
+        assert_response :success
+
+        data = JSON.parse @response.body
+        data.length.must_equal Customer.count
+
+        # Verify sorted order
+        data.each_with_index do |customer, i|
+          if i + 1 >= data.length
+            break
+          end
+
+          customer['name'].must_be :<=, data[i+1]['name']
+        end
+      end
+
+      it "can sort by registered_at" do
+        get customers_url, params: { sort: 'registered_at' }
+        assert_response :success
+
+        data = JSON.parse @response.body
+        data.length.must_equal Customer.count
+
+        # Verify sorted order
+        data.each_with_index do |customer, i|
+          if i + 1 >= data.length
+            break
+          end
+
+          DateTime.parse(customer['registered_at']).must_be :<=, DateTime.parse(data[i+1]['registered_at'])
+        end
+      end
+
+      it "can sort by postal_code" do
+        get customers_url, params: { sort: 'postal_code' }
+        assert_response :success
+
+        data = JSON.parse @response.body
+        data.length.must_equal Customer.count
+
+        # Verify sorted order
+        data.each_with_index do |customer, i|
+          if i + 1 >= data.length
+            break
+          end
+
+          customer['postal_code'].must_be :<=, data[i+1]['postal_code']
+        end
+      end
+
+      it "returns an error for an invalid sort field" do
+        get customers_url, params: { sort: 'gnome' }
+        assert_response :bad_request
+
+        data = JSON.parse @response.body
+        data.must_be_kind_of Hash
+
+        data.must_include 'errors'
+        data['errors'].must_include 'sort'
       end
     end
   end
