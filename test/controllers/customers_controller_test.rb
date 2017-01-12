57f27b62ba05f524de9c4e19eae1b97671f5d589
Dan Roberts <dan@adadevelopersacademy.org>
2017-01-11 16:09:43 -0800

    Initial work on tests for /customers

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -11,5 +11,33 @@ class CustomersControllerTest < ActionController::TestCase
       data = JSON.parse @response.body
       data.must_be_kind_of Array
     end
+
+    it "should return many customer fields" do
+      get :index
+      assert_response :success
+
+      data = JSON.parse @response.body
+      data.each do |customer|
+        customer.must_include "name"
+        customer.must_include "registered_at"
+        customer.must_include "postal_code"
+        customer.must_include "phone"
+        customer.must_include "account_credit"
+        customer.must_include "movies_checked_out"
+      end
+    end
+
+    it "returns all customers when no query params are given" do
+      get :index
+      assert_response :success
+
+      data = JSON.parse @response.body
+      data.length.must_equal Customer.count
+
+      expected_names = Customer.all.map do |customer|
+        customer.name
+      end
+
+    end
   end
 end
