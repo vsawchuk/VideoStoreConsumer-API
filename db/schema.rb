6993609989ad9b7c4083a8f68050594396074115
Kari Bancroft <kariabancroft@gmail.com>
2017-06-08 13:20:27 -0700

    Add image field to movie table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118201517) do
+ActiveRecord::Schema.define(version: 20170608201920) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170118201517) do
     t.integer  "inventory"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "image_url"
   end
 
   create_table "rentals", force: :cascade do |t|
