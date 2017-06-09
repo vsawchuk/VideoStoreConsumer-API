35d662ea3f0cf128dbaa85a7a8196dbe0385c482
Kari Bancroft <kariabancroft@gmail.com>
2017-06-08 22:12:23 -0700

    Reset the default img url

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,19 @@
 class Movie < ApplicationRecord
   attr_accessor :external_id
-  
+
   has_many :rentals
   has_many :customers, through: :rentals
 
   def available_inventory
     self.inventory - Rental.where(movie: self, returned: false).length
+  end
+
+  def image_url
+    orig_value = read_attribute :image_url
+    if !orig_value
+      MovieWrapper::DEFAULT_IMG_URL
+    else
+      MovieWrapper.construct_image_url(orig_value)
+    end
   end
 end
