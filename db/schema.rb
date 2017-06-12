7286fe3c7edf05f53b828bf9cbafa041602a8c70
Chris <chris@adadevelopersacademy.org>
2017-06-12 13:11:55 -0700

    Added default value to schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608201920) do
+ActiveRecord::Schema.define(version: 20170612201103) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 20170608201920) do
     t.integer  "movie_id"
     t.date     "checkout_date"
     t.date     "due_date"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.boolean  "returned"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.boolean  "returned",      default: false
     t.index ["customer_id"], name: "index_rentals_on_customer_id"
     t.index ["movie_id"], name: "index_rentals_on_movie_id"
   end
