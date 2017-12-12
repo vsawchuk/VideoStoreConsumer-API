adc28732877fb381d88ae34af7ba07ac414e5566
Kari Bancroft <kariabancroft@gmail.com>
2017-12-12 09:56:38 -0800

    update seeds to use the search rather than the default image url. this will take a bit longer but be less odd when demoing

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ customer_data.each do |customer|
   Customer.create!(customer)
 end
 
-JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
-  Movie.create!(movie)
+JSON.parse(File.read('db/seeds/movies.json')).each do |movie_data|
+  movies = MovieWrapper.search(movie_data["title"])
+  movies.first.save unless movies.empty?
 end
