793143f845be936b8ef1013d233d2331aec63142
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-12-19 14:05:11 -0800

    Add post request functionality to create rental movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,6 +21,23 @@ class MoviesController < ApplicationController
       )
   end
 
+  def create
+    movie = Movie.new(movie_params)
+    if movie.save
+      render(
+        json: movie.as_json(only: [:id]),
+        status: :ok
+      )
+    else
+      render(
+        json: {
+          "ok" => false,
+          "errors" => movie.errors.messages
+        },
+        status: :bad_request) #400 response code
+    end
+  end
+
   private
 
   def require_movie
@@ -28,5 +45,9 @@ class MoviesController < ApplicationController
     unless @movie
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
+  end
+
+  def movie_params
+    params.permit(:title, :overview, :release_date, :inventory, :external_id, :image_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get "/movies/search", to: "movies#index", as: 'search'
   resources :movies, only: [:index, :show], param: :title
+  post "/movies", to: "movies#create", as: 'add_movie'
 
   post "/rentals/:title/check-out", to: "rentals#check_out", as: "check_out"
   post "/rentals/:title/return", to: "rentals#check_in", as: "check_in"
