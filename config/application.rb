21ebae26cbc2448e8baa03d5d65d9daf8d51c171
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-12-19 15:18:43 -0800

    Add rackcors gem and allow post requests from our JS app

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,15 @@ module VideoStoreAPIRails
     #this loads everything in the lib folder automatically
     config.eager_load_paths << Rails.root.join('lib')
 
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'http://localhost:8080',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-    }
+    # config.action_dispatch.default_headers = {
+    #   'Access-Control-Allow-Origin' => 'http://localhost:8080',
+    #   'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    # }
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
